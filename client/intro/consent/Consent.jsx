df4feccbcaabfba75aba10f7a99ead8e31cde3da
Samuel Dupret <samu.dupret@hotmail.com>
2020-10-23 11:10:27 +0100

    accessible only with chrome

--- client/intro/consent/Consent.jsx
+++ client/intro/consent/Consent.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { Centered, ConsentButton } from "meteor/empirica:core";
-import { isMobile, isFirefox, isChrome } from 'react-device-detect';
+import { isMobile, isChrome } from 'react-device-detect';
 
 export default class Consent extends React.Component {
 	render() {
-		return !isMobile && (isFirefox || isChrome) ?
+		return !isMobile && isChrome ?
 			(
 				<Centered>
 					<div>
 						<h2> Consent Form </h2>
 						<p>
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec imperdiet molestie ante ut placerat. Curabitur nec velit arcu. Proin sagittis porta ligula sit amet feugiat. Nunc molestie pharetra orci, a tristique tortor. Sed sodales risus at sapien ultricies scelerisque. Fusce id ornare diam, eu efficitur ipsum. Vivamus eleifend maximus lectus eget semper. Aenean vel velit non mauris rutrum suscipit a sollicitudin metus. Fusce pharetra ac purus ac interdum. In posuere mattis ultrices. Mauris sed laoreet ipsum.
-         			 </p>
+         			 	</p>
+
 						<p>
 							In vitae sollicitudin arcu. Etiam a tincidunt ante, eget porttitor lectus. Sed vulputate ligula sem, vitae semper lorem rhoncus euismod. Nam non egestas dui, vitae pulvinar neque. Praesent varius vulputate nisl, faucibus pretium erat auctor vel. Pellentesque tempus ligula et lectus sollicitudin semper. Nam sit amet velit velit. Curabitur luctus tincidunt accumsan. Donec ut est in nunc bibendum laoreet. Quisque faucibus turpis ac aliquam viverra. Cras ac justo ullamcorper diam rutrum elementum. Nulla in consectetur ipsum. Morbi placerat, nibh quis convallis ornare, tortor justo finibus leo, hendrerit mattis arcu ante vitae turpis. Vestibulum ac semper magna. Vestibulum ultrices, eros quis rhoncus tincidunt, mi lorem gravida neque, a sollicitudin nibh nunc in tellus.
-          			</p>
+          				</p>
+
+						<p>
+							This study cannot run on mobile nor on browsers other than Chrome. Please make sure you are using a <strong>computer</strong> and that you are using <strong>Chrome</strong>.
+         			 	</p>
 
 						<br />
 						<ConsentButton text="I AGREE" />
@@ -26,7 +31,7 @@ export default class Consent extends React.Component {
 					<div>
 						<h2> Consent Form </h2>
 						<p>
-							This study cannot run on mobile, nor on browsers other than Firefox or Chrome. Please make sure you are using a <strong>computer</strong> and that you are using <strong>Firefox or Chrome</strong>.
+							This study cannot run on mobile nor on browsers other than Chrome. Please make sure you are using a <strong>computer</strong> and that you are using <strong>Chrome</strong>.
          			 	</p>
 					</div>
 				</Centered>
